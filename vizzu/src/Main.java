8b7ff2728a64ad571dcaf829b693fe64f8b59f42
Igor Barbosa <igorbarbosaws@gmail.com>
2025-10-15 00:40:51 -0300

    Update Main.java

--- vizzu/src/Main.java
+++ vizzu/src/Main.java
@@ -1,12 +1,13 @@
-public class Main {
-    static void main(String[] args){
-        System.out.println("Olá, esse é o Vizzu!");
-        System.out.println("Filme: The Godfather");
+void main() {
+    IO.println("Olá, esse é o Vizzu!");
+    IO.println("Filme: The Godfather");
 
-        int anoDeLancamento = 1972;
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-        boolean incluidoNoPlano = true;
-        double notaDoFilme = 9.2;
-        System.out.println("IMDB = " + notaDoFilme);
-    }
+    int anoDeLancamento = 1972;
+    double notDoFilme = 9.2;
+    String protagonista = "Al Pacino";
+    String coadjuvante = "Marlon Brando";
+
+    IO.println("Ano de lançamento: " + anoDeLancamento);
+    IO.println("Elenco: " + protagonista + ", " + coadjuvante);
+    IO.println("IMDB = " + notDoFilme);
 }
