f02b95a988d84285e34c3f0120e9639ca720a34c
Igor Barbosa <igorbarbosaws@gmail.com>
2025-10-14 23:58:42 -0300

    Uploado files
    Novo projeto.

---
+++ vizzu/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    static void main(String[] args){
+        System.out.println("Olá, esse é o Vizzu!");
+        System.out.println("Filme: The Godfather");
+
+        int anoDeLancamento = 1972;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notDoFilme = 9.2;
+        System.out.println("IMDB = " + notDoFilme);
+    }
+}
